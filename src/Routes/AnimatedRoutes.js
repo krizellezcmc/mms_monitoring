bc3cb440d7713c4608363017b5f496bb462873f4
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2022-12-16 16:02:54 +0800

    Krizelle

--- src/Routes/AnimatedRoutes.js
+++ src/Routes/AnimatedRoutes.js
@@ -3,6 +3,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import useAuth from "../Hooks/useAuth";
 import { AnimatePresence } from "framer-motion";
 import PurchaseOrder from "../Pages/PurchaseOrder";
+import POReport from "../Components/POReport";
 
 function AnimatedRoutes(props) {
   return (
@@ -10,6 +11,7 @@ function AnimatedRoutes(props) {
       <Routes>
         <Route path="/"></Route>
         <Route path="/po" element={<PurchaseOrder />}></Route>
+        <Route path="/po-report" element={<POReport />}></Route>
       </Routes>
     </AnimatePresence>
   );
