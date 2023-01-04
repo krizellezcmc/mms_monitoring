ee1f9709a08fc83d0c2085720dd244d00d842cb5
krizellezcmc <kmfalcasantos.zcmc@gmail.com>
2023-01-04 09:38:19 +0800

    kriz

--- src/Pages/PurchaseOrder.js
+++ src/Pages/PurchaseOrder.js
@@ -20,7 +20,6 @@ import {
   BsFillCalendar2CheckFill,
   BsFillCalendar2Fill,
   BsFillCalendar2RangeFill,
-  BsArrowRightShort,
   BsBox,
 } from "react-icons/bs";
 import BarChart from "../Components/Charts/BarChart";
