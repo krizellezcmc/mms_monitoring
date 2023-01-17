6bfaa49bce95ec0707d2373d9efe6381b30870ed
TjayAmit <tristanjayamit0813@gmail.com>
2023-01-17 08:34:24 +0800

    update

--- src/Pages/Main_Layout.js
+++ src/Pages/Main_Layout.js
@@ -1,9 +1,14 @@
 import SidebarComponent from "../Components/Sidebar_Comonent";
 import PurchaseRequest from "./PurchaseRequest";
+import SideDrawer from "../Components/SideDrawer";
+import { useDisclosure } from "@chakra-ui";
 
 const MainLayout = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
+      <SideDrawer />
       <SidebarComponent children={<PurchaseRequest />} />
     </>
   );
