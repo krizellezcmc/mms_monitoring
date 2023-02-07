322ba1094e62597b629be9c6805aadc4dc8189ce
Alyana Barretto <alyanabarretto.zcmc@gmail.com>
2023-02-07 14:38:33 +0800

    yana

--- src/Components/IssuedByCategory.js
+++ src/Components/IssuedByCategory.js
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Select } from "chakra-react-select";
 import {
-  Box,
-  Flex,
-  Spacer,
-  Text,
   Table,
   Tr,
   Td,
@@ -12,18 +7,27 @@ import {
   TableContainer,
   Thead,
   Tbody,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Text,
+  Spacer,
+  HStack,
+  Input,
   InputGroup,
   InputLeftElement,
-  Input,
-  Button,
-  Badge,
-  Icon,
+  Flex,
+  Center,
+  Box,
 } from "@chakra-ui/react";
 import localApi from "../API/localAPI";
-import NoSelectedItem from "./NoSelectedItem";
 import { BiSearch } from "react-icons/bi";
-import { BsArrowDown, BsArrowDownShort, BsArrowUpShort } from "react-icons/bs";
-import moment from "moment";
 
 function IssuedByCategory(props) {
   // const [options, setOptions] = useState([]);
@@ -34,7 +38,20 @@ function IssuedByCategory(props) {
   // const [category, setCategory] = useState("");
   // const [search, setSearch] = useState("");
   const [issued, setIssued] = useState([]);
-
+  const [items, setItems] = useState([]);
+  const [itemsIssued, setItemsIssued] = useState([]);
+  const [label, setLabel] = useState("");
+  const {
+    isOpen: isTotalOpen,
+    onOpen: onTotalOpen,
+    onClose: onTotalClose,
+  } = useDisclosure();
+  const {
+    isOpen: isIssuedOpen,
+    onOpen: onIssuedOpen,
+    onClose: onIssuedClose,
+  } = useDisclosure();
+  const [search, setSearch] = useState("");
   // const getItemList = async () => {
   //   let response = await localApi.get("/get_itemList.php", {
   //     params: { category: props.id },
@@ -68,7 +85,21 @@ function IssuedByCategory(props) {
 
   // let balance = total - issued;
   // let limit = total * 0.25;
+  const getItems = async (cat) => {
+    let response = await localApi.get("/get_ItemsbyCategory.php", {
+      params: { itemCat: cat },
+    });
+    setItems(response.data);
+  };
 
+  const getIssuedItems = async (cat) => {
+    let res = await localApi.get("/get_ItemsIssuedbyCat.php", {
+      params: { itemCat: cat },
+    });
+    setItemsIssued(res.data);
+
+    console.log(res.data);
+  };
   useEffect(() => {
     const getList = async () => {
       let response = await localApi.get("/get_TotalbyCat.php");
@@ -82,6 +113,7 @@ function IssuedByCategory(props) {
 
     getList();
     getIssued();
+
     // getTotal();
 
     // const loop = () => {
@@ -102,7 +134,7 @@ function IssuedByCategory(props) {
   return (
     <div>
       <TableContainer mt={50} display="flex" w={1200}>
-        <Table variant="striped">
+        <Table variant="simple">
           <Thead>
             <Tr>
               <Th textAlign="left" w={600}>
@@ -116,9 +148,23 @@ function IssuedByCategory(props) {
               return (
                 <>
                   <Tr key={k}>
-                    <Td fontWeight={500}>{j.desc}</Td>
-                    <Td textAlign="center" width="200px">
-                      {Math.round(j.total).toLocaleString()}
+                    <Td fontWeight={500}> {j.desc}</Td>
+                    <Td
+                      textAlign="center"
+                      width="200px"
+                      _hover={{ background: "gray.100" }}
+                    >
+                      <Button
+                        variant="link"
+                        colorScheme="teal"
+                        onClick={() => {
+                          setLabel(j.desc);
+                          getItems(j.value);
+                          onTotalOpen();
+                        }}
+                      >
+                        {Math.round(j.total).toLocaleString()}
+                      </Button>
                     </Td>
                   </Tr>
                 </>
@@ -127,7 +173,7 @@ function IssuedByCategory(props) {
           </Tbody>
         </Table>
 
-        <Table variant="striped" w={200}>
+        <Table variant="simple" w={200}>
           <Thead>
             <Tr>
               <Th textAlign="center">Stocks Issued</Th>
@@ -137,9 +183,19 @@ function IssuedByCategory(props) {
             {issued.map((j, k) => {
               return (
                 <>
-                  <Tr>
+                  <Tr _hover={{ background: "gray.100" }}>
                     <Td fontWeight={500} textAlign="center">
-                      {Math.round(j.total).toLocaleString()}
+                      <Button
+                        variant="link"
+                        colorScheme="teal"
+                        onClick={() => {
+                          setLabel(j.desc);
+                          getIssuedItems(j.value);
+                          onIssuedOpen();
+                        }}
+                      >
+                        {Math.round(j.total).toLocaleString()}
+                      </Button>
                     </Td>
                   </Tr>
                 </>
@@ -147,7 +203,7 @@ function IssuedByCategory(props) {
             })}
           </Tbody>
         </Table>
-        <Table variant="striped" w={200}>
+        <Table variant="simple" w={200}>
           <Thead>
             <Tr>
               <Th textAlign="center">Balance</Th>
@@ -168,6 +224,156 @@ function IssuedByCategory(props) {
           </Tbody>
         </Table>
       </TableContainer>
+
+      <Modal
+        onClose={onTotalClose}
+        isOpen={isTotalOpen}
+        scrollBehavior="outside"
+        size="full"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign="center">
+            {label}
+            <Text fontSize={16} mt={10}>
+              Total Stocks
+            </Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Center>
+              <Input
+                fontSize="13px"
+                type="text"
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search item/unit..."
+                width="400px"
+                _hover={{ borderColor: "green" }}
+                _focus={{
+                  boxShadow: "none",
+                  outline: "none",
+                  borderColor: "green",
+                }}
+              />
+            </Center>
+
+            <TableContainer w={1000} margin="auto" display="flex">
+              <Table variant="striped" colorScheme="cyan" size="sm" mt={10}>
+                <Thead>
+                  <Tr fontSize={11} fontWeight={500}>
+                    <Th width="200px" textAlign="center">
+                      ITEM DESCRIPTION
+                    </Th>
+                    <Th textAlign="center">UNIT</Th>
+                    <Th textAlign="center">QUANTITY</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {items
+                    .filter((val) => {
+                      if (search === "") {
+                        return val;
+                      } else if (
+                        val.desc.toLowerCase().includes(search.toLowerCase()) ||
+                        val.unit.toLowerCase().includes(search.toLowerCase())
+                      ) {
+                        return val;
+                      }
+                    })
+                    .map((j, k) => {
+                      return (
+                        <>
+                          <Tr fontSize={11}>
+                            <Td>{j.desc}</Td>
+                            <Td textAlign="center">{j.unit}</Td>
+                            <Td textAlign="center">{j.total}</Td>
+                          </Tr>
+                        </>
+                      );
+                    })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onTotalClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      <Modal
+        onClose={onIssuedClose}
+        isOpen={isIssuedOpen}
+        scrollBehavior="outside"
+        size="full"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader textAlign="center">
+            {label}{" "}
+            <Text fontSize={16} mt={10}>
+              Issued Stocks
+            </Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Input
+              fontSize="13px"
+              type="text"
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search item/unit..."
+              width="400px"
+              _hover={{ borderColor: "green" }}
+              _focus={{
+                boxShadow: "none",
+                outline: "none",
+                borderColor: "green",
+              }}
+            />
+
+            <TableContainer w={1000} margin="auto" display="flex">
+              <Table variant="striped" colorScheme="cyan" size="sm" mt={10}>
+                <Thead>
+                  <Tr fontSize={11} fontWeight={500}>
+                    <Th width="200px" textAlign="center">
+                      ITEM DESCRIPTION
+                    </Th>
+                    <Th textAlign="center">UNIT</Th>
+                    <Th textAlign="center">QUANTITY</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {itemsIssued
+                    .filter((val) => {
+                      if (search === "") {
+                        return val;
+                      } else if (
+                        val.desc.toLowerCase().includes(search.toLowerCase()) ||
+                        val.unit.toLowerCase().includes(search.toLowerCase())
+                      ) {
+                        return val;
+                      }
+                    })
+                    .map((j, k) => {
+                      return (
+                        <>
+                          <Tr fontSize={11}>
+                            <Td>{j.desc}</Td>
+                            <Td textAlign="center">{j.unit}</Td>
+                            <Td textAlign="center">{j.total}</Td>
+                          </Tr>
+                        </>
+                      );
+                    })}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onIssuedClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
